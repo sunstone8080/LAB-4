dfcdca9d3ba8df7a35d9e3a9e8a68efa03d902c4
Chase Davidson <chasebdavidson@gmail.com>
2026-02-13 19:38:26 -0500

    Update hpCalculator.cs

--- hpCalculator.cs
+++ hpCalculator.cs
@@ -12,8 +12,8 @@ public class SolutionOne : MonoBehaviour
     public bool tough;
     public bool stout;
     // t average f roll
-    public bool average; 
-    
+    public bool average;
+
     [Header("Output")]
     public int finalHP;
 
@@ -21,76 +21,80 @@ public class SolutionOne : MonoBehaviour
     private Dictionary<string, int> classHitDice = new Dictionary<string, int>();
     private Dictionary<string, int> racialBonus = new Dictionary<string, int>();
 
-    private void Awake()
+    private void InitializeDictionaries()
     {
-        //class die
-        classHitDice.Add("Barbarian", 12);
-        classHitDice.Add("Fighter", 10);
-        classHitDice.Add("Paladin", 10);
-        classHitDice.Add("Ranger", 10);
-        classHitDice.Add("Bard", 8);
-        classHitDice.Add("Cleric", 8);
-        classHitDice.Add("Druid", 8);
-        classHitDice.Add("Monk", 8);
-        classHitDice.Add("Rogue", 8);
-        classHitDice.Add("Warlock", 8);
-        classHitDice.Add("Sorcerer", 6);
-        classHitDice.Add("Wizard", 6);
-        //racial
-        racialBonus.Add("Dwarf", 2);
-        racialBonus.Add("Orc", 1);
-        racialBonus.Add("Goliath", 1);
+        classHitDice = new Dictionary<string, int>()
+    {
+        {"Barbarian", 12},
+        {"Fighter", 10},
+        {"Paladin", 10},
+        {"Ranger", 10},
+        {"Bard", 8},
+        {"Cleric", 8},
+        {"Druid", 8},
+        {"Monk", 8},
+        {"Rogue", 8},
+        {"Warlock", 8},
+        {"Sorcerer", 6},
+        {"Wizard", 6}
+    };
+
+        racialBonus = new Dictionary<string, int>()
+    {
+        {"Dwarf", 2},
+        {"Orc", 1},
+        {"Goliath", 1}
+    };
     }
 
-    private void OnValidate()
+
+    private void Start()
     {
-        CalculateHP();
+        calculateHP();
+        Debug.Log(printStats());
     }
 
-    public void CalculateHP()
+    public void calculateHP()
     {
-        //clamp
+        // Make sure dictionaries exist (important for OnValidate)
+        if (classHitDice == null || classHitDice.Count == 0)
+            InitializeDictionaries();
+
         if (level < 1) level = 1;
         if (level > 20) level = 20;
 
-        //con mod
-        int conMod = Mathf.FloorToInt((conScore - 10) / 2f);
+        // SAFETY: prevent null keys
+        int raceHP = 0;
+        if (!string.IsNullOrEmpty(race) && racialBonus.ContainsKey(race))
+            raceHP = racialBonus[race];
 
-        //total hitdie
-        int hitDie = classHitDice.ContainsKey(characterClass) ? classHitDice[characterClass] : 8;
+        int totalCon = conScore + raceHP;
+        int conMod = Mathf.FloorToInt((totalCon - 10) / 2f);
 
-        //racial bonus
-        int raceHP = racialBonus.ContainsKey(race) ? racialBonus[race] : 0;
+        int hitDie = 8; // default
+        if (!string.IsNullOrEmpty(characterClass) && classHitDice.ContainsKey(characterClass))
+            hitDie = classHitDice[characterClass];
 
-        //feats
         int featHP = 0;
-        if (tough) featHP += 2;
-        if (stout) featHP += 1;
+        if (tough) featHP += 2 * level;
 
         int total = 0;
 
-        //level 1
-        total += hitDie + conMod + raceHP + featHP;
+        total += hitDie + conMod;
 
-        //higher levels
         for (int i = 2; i <= level; i++)
         {
-            int roll;
+            int roll = average ? (hitDie / 2) + 1
+                               : Random.Range(1, hitDie + 1);
 
-            if (average)
-            {
-                roll = Mathf.RoundToInt((hitDie + 1) / 2f);
-            }
-            else
-            {
-                roll = Random.Range(1, hitDie + 1);
-            }
-
-            finalHP = total + roll + conMod + raceHP + featHP;
+            total += roll + conMod;
         }
+
+        finalHP = total + featHP;
     }
 
-    private string BuildSummary()
+
+    private string printStats()
     {
         string toughText = tough ? "has" : "does not have";
         string stoutText = stout ? "has" : "does not have";
